c28fe4423a0590b8a9bb9191639f369217084117
ahmedrjba15-cell <ahmedrjba15@gmail.com>
2026-05-12 20:54:12 +0300

    Create omni_tb7104.mk

---
+++ omni_tb7104.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+PRODUCT_DEVICE := tb7104
+PRODUCT_NAME := omni_tb7104
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo Tab E7
+PRODUCT_MANUFACTURER := lenovo
+
+TARGET_DEVICE := tb7104
